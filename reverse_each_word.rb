8a9607df5bf0efee7bdf15c914e4534716352e5c
sarah <s.j.condon01@gmail.com>
2019-09-19 03:02:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,20 @@
-def  reverse_each_word(string)
+def  reverse_each_word_with_eah(string)
+  orginal_array = string.split(" ")
+  return_array = []
+  orginal_array.each do|string| 
+  return array << string.reverse
+end
   reverse_each_word.each do |string|
     end
-    puts reverse_each_word ("I really enjoy you")
+  return.array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split (" ")
+  test_array = []
+  array.collect do|string|
+    test.array << string.reverse_each_word
+  end
+    test_array.join(" ")
+  end
+    reverse_each_word ("I really enjoy you")
