d0b9f7a1abf691155ff6ea246618ed38b19a7c38
sarah <s.j.condon01@gmail.com>
2019-09-19 03:17:23 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def  reverse_each_word_with_eah(string)
+  orginal_array = string.split(" ") 
+  return_array = []
+  orginal_array.each do|string| 
+  return array << string.reverse 
+
+end
+  return_array.join(" ") 
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse
+  end
+    test_array.join(" ")
+  end
+    reverse_each_word ("I really enjoy you")
+  
+  
