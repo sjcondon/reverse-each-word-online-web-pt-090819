460ac07f12eb35a6b9f2789484833b72d4181cff
sarah <s.j.condon01@gmail.com>
2019-09-19 03:06:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
   array.collect do|string|
-    test_array << string.reverse_each_word
+    test_array << string.reverse
   end
     test_array.join(" ")
   end
